34bce60fa0341bc9b19854dbfbd0449247238f90
Valerio Bozzolan <boz@reyboz.it>
2020-06-06 14:46:04 +0200

    Improve default example

--- load-example.php
+++ load-example.php
@@ -9,8 +9,8 @@ define( 'ABSPATH', __DIR__ );
 
 // load the boz-mw framework
 //    git clone https://gitpull.it/source/boz-mw/
-require '/usr/share/php/mediawiki/boz-mw/autoload.php';
+require __DIR__  . '/../boz-mw/autoload.php';
 
 // load the suckless-php framework
 //    git clone https://gitpull.it/source/suckless-php/
-require '/usr/share/php/suckless-php/load.php';
+require __DIR__ . '/../suckless-php/load.php';
